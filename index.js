2c0b1d0e359b4fae77aff9732c013d7df449e0c3
Lasya Venna <lasya@metrohub.io>
2026-02-01 20:05:56 -0800

    added a hamburger menu with sections leading to "avatar", "profile" and "settings"

--- index.js
+++ index.js
@@ -64,3 +64,11 @@ taskField.addEventListener("keydown", (e) => {
         addBtn.click();
     }
 });
+
+// hamburger menu
+const hamburger = document.getElementById("hamburger");
+const sideMenu = document.getElementById("side-menu");
+
+hamburger.addEventListener("click", () => {
+    sideMenu.classList.toggle("open");
+});
